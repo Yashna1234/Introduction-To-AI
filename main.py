d37d0304aec96e8b58426a38d74ea9b3ca5cb7e0
Yashna1234 <khubchandaniyashna@gmail.com>
2026-04-20 20:57:20 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello, my name is chatbot nice to see you, Whats your name?")
+name=input(": ")
+print(f"nice to meet you {name}")
+print("how may i assist you today? ")
+answer=input(": ")
+if answer=="whats an ai":
+    print("AI stands for Artificial Intelligence")
+print("goodbye")
